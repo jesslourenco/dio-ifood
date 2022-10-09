845d627aed685067266990cf3d75c6c9c363ca19
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-10-08 23:56:20 -0400

    design pattern challenge

---
+++ desing-patterns-java/src/main/java/me/dio/designpatterns/Facade.java
@@ -0,0 +1,13 @@
+package me.dio.designpatterns;
+import sub2.CepApi;
+import sub1.CrmService;
+
+public class Facade {
+
+    public void migrarCliente(String nome, String cep) {
+        String cidade = CepApi.getInstancia().recuperarCidade(cep);
+        String estado = CepApi.getInstancia().recuperarEstado(cep);
+
+        CrmService.gravarCliente(nome, cep, cidade, estado);
+    }
+}
