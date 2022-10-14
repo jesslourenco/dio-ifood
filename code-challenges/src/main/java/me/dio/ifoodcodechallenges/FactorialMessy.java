891f0d70c3f2140bb5a3d2b3d952bff6f89ec2da
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2022-10-13 22:15:01 -0400

    challenge messy factorial

---
+++ code-challenges/src/main/java/me/dio/ifoodcodechallenges/FactorialMessy.java
@@ -0,0 +1,36 @@
+package me.dio.ifoodcodechallenges;
+
+import java.util.Scanner;
+
+public class FactorialMessy {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(new Scanner(System.in).nextLine());
+        int res = 0;
+        int cnt = 1;
+        int tmp = n;
+        n--;
+        while (n > 0) {
+            tmp = tmp * n;
+            n--;
+
+            if(n != 0){
+                tmp = tmp / n;
+                n--;
+            }
+
+            tmp += n;
+            n--;
+
+            res += tmp;
+
+            tmp = -n;
+
+            if (n == 1 ){
+                res += tmp;
+            }
+
+            n--;
+        }
+        System.out.println(res);
+    }
+}
